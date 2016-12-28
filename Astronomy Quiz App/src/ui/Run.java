fe5b15ef6f1c98be61b067092a1a253185e779e4
Ahmed Ghannam <amalghannam@crimson.ua.edu>
2016-12-28 00:30:46 -0800

    Added some comments.

--- Astronomy Quiz App/src/ui/Run.java
+++ Astronomy Quiz App/src/ui/Run.java
@@ -1,5 +1,12 @@
+/*
+ * File: Run.java
+ */
 package ui;
 
+/**
+ * This class is the main driver through which to run the application. 
+ * @author Ahmed Ghannam 
+ */
 public class Run {
 
 	public static void main(String[] args) {
